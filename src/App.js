e4cb4b0c39dfaa78391bc5eb049a870ce7e12f0e
celirojas <mcelinarojas@gmail.com>
2022-06-11 09:42:34 -0300

    subo proyecto terminado

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import Pokedex from './Pokedex';
+
+class App extends Component{
+    render(){
+        return <div className='App'>
+            <Pokedex />
+        </div>
+    }
+}
+
+export default App;
